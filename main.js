c1baa4f02cbb61359ed925ab18381ec326d872df
alaswadiyy <abdulmuizsharafadeen@gmail.com>
2022-03-09 10:08:43 +0100

    new commit

--- main.js
+++ main.js
@@ -30,35 +30,96 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
-const skillsContent = document.getElementsByClassName('skills__content'),
-      skillsHeader = document.querySelectorAll('.skills__header')
+// const skillsContent = document.getElementsByClassName('skills__content'),
+//       skillsHeader = document.querySelectorAll('.skills__header')
 
-function toggleSkills(){
-    let itemClass = this.parentNode.getElementsByClassName
+// function toggleSkills(){
+//     let itemClass = this.parentNode.ClassName
 
-    for(i=0; 1 < skillsContent.length; i++){
-        skillsContent[i].className = 'skills__content skills__close'
-    }
-    if(itemClass === 'skills__content skills__close'){
-        this.parentNode.className = 'skills__content skills__open'
-    }
-}
+//     for(i = 0; i < skillsContent.length; i++) {
+//         skillsContent[i].className = 'skills__content skills__close'
+//     }
+//     if(itemClass === 'skills__content skills__close'){
+//         this.parentNode.className = 'skills__content skills__open'
+//     }
+// }
 
-skillsHeader.forEach((el) =>{
-    el.addEventListener('click', toggleSkills)
-})
+// skillsHeader.forEach((el) =>{
+//     el.addEventListener('click', toggleSkills)
+// })
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__action')
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
 
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
 
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalView.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiperPortfolio = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
 /*==================== TESTIMONIAL ====================*/
-
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+    
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView: 2,
+        }
+    }
+  });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
